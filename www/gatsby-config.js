cc3b6d7787f369209eaece5345a69bf3038ed70b
gianfaye <git@gianfaye.com>
2020-09-07 03:08:58 +0800

    Add sitemap

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -88,6 +88,10 @@ const plugins = [
         "https://narative.us19.list-manage.com/subscribe/post?u=65ef169332a03669b9538f6ef&amp;id=c55c426282",
     },
   },
+  {
+    resolve: `gatsby-plugin-sitemap`,
+    options: {},
+  },
 ];
 
 /**
